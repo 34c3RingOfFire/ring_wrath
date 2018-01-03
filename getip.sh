0a8f0c6dae2748a92ff3e5cfc67a4d0139c76b58
paradx <paradx@users.noreply.github.com>
2018-01-03 14:33:23 +0100

    Update getip.sh
    added Comment

--- getip.sh
+++ getip.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+#This will return the local IP adress(es) and filters those that aren't usefull (127.0.0.1, 10.X.X.X, etc)
 ifconfig | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' | tail -n 2 | head -n 1
